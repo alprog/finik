e25e278a19c3575fc99157aa867fc3afd5df2a56
Alexander Tuzhik <alprog@hotmail.com>
2026-04-29 01:12:40 +0200

    fix icon

--- source/DesktopWindow.cpp
+++ source/DesktopWindow.cpp
@@ -10,23 +10,27 @@ import Gui;
 import Images;
 import ByteBlob;
 
+void FlushSDLEvents()
+{
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {}
+}
+
 DesktopWindow::DesktopWindow(int width, int height)
     : width{width}
     , height{height}
 {
-    SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIGH_PIXEL_DENSITY | SDL_WINDOW_MAXIMIZED);
+    SDL_WindowFlags window_flags = (SDL_WindowFlags)( SDL_WINDOW_RESIZABLE | SDL_WINDOW_HIGH_PIXEL_DENSITY | SDL_WINDOW_MAXIMIZED);
     impl = SDL_CreateWindow("title", 0, 0, window_flags);
+    id = SDL_GetWindowID(impl);
     SDL_GetWindowSize(impl, &this->width, &this->height);
 
-    id = SDL_GetWindowID(impl);
-
+    setIcon(); 
+    
     SDL_PropertiesID properties = SDL_GetWindowProperties(impl);
     hwnd = (HWND)SDL_GetPointerProperty(properties, SDL_PROP_WINDOW_WIN32_HWND_POINTER, nullptr);
-
     swap_chain = new SwapChain(*this);
     gui = new Gui(*this);
-
-    setIcon();
 }
 
 void DesktopWindow::setIcon()
@@ -36,7 +40,9 @@ void DesktopWindow::setIcon()
     int32 w = image->width;
     int32 h = image->height;
     SDL_Surface* icon = SDL_CreateSurfaceFrom(w, h, SDL_PIXELFORMAT_RGBA32, image->data, w * 4);
-    SDL_SetWindowIcon(impl, icon);
+    SDL_SetWindowIcon(impl, icon);   
+
+    FlushSDLEvents(); // This needed for taskbar icon
 }
 
 DesktopWindow::~DesktopWindow()
