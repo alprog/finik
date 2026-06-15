339b4dec18e8557e7f92b667e16fd24a1e2d95d5
Alexander Tuzhik <alprog@hotmail.com>
2026-06-15 22:20:50 +0200

    fix box

--- source/Mesh.cpp
+++ source/Mesh.cpp
@@ -7,20 +7,20 @@ Mesh* createCubeMesh()
     MeshBuilder builder;
 
     auto add = [&builder](Vector3 of1, Vector3 of2, Vector3 normal, Vector2 uv) {
-        StandardVertex a{-of1 - of2 + normal, normal, uv};
-        StandardVertex b{of1 - of2 + normal, normal, uv + Vector2(0.33f, 0.0f)};
-        StandardVertex c{of1 + of2 + normal, normal, uv + Vector2(0.33f, 0.5f)};
-        StandardVertex d{-of1 + of2 + normal, normal, uv + Vector2(0, 0.5f)};
+        StandardVertex a{-of1 + of2 + normal, normal, uv};
+        StandardVertex b{of1 + of2 + normal, normal, uv + Vector2(0.33f, 0.0f)};
+        StandardVertex c{of1 - of2 + normal, normal, uv + Vector2(0.33f, 0.5f)};
+        StandardVertex d{-of1 - of2 + normal, normal, uv + Vector2(0, 0.5f)};
         builder.addQuad(a, b, c, d);
     };
 
-    add(Vector3::Left, Vector3::Up, Vector3::Forward, Vector2(0, 0.5f));
-    add(Vector3::Forward, Vector3::Up, Vector3::Right, Vector2(0.33f, 0.5f));
-    add(Vector3::Left, Vector3::Backward, Vector3::Up, Vector2(0.66f, 0.5f));
+    add(Vector3::Left, Vector3::Up, Vector3::Forward, Vector2(0, 0));
+    add(Vector3::Forward, Vector3::Up, Vector3::Right, Vector2(0.33f, 0));
+    add(Vector3::Left, Vector3::Backward, Vector3::Up, Vector2(0.66f, 0));
 
-    add(Vector3::Right, Vector3::Up, Vector3::Backward, Vector2(0, 0));
-    add(Vector3::Backward, Vector3::Up, Vector3::Left, Vector2(0.33f, 0));
-    add(Vector3::Right, Vector3::Backward, Vector3::Down, Vector2(0.66f, 0));
+    add(Vector3::Right, Vector3::Up, Vector3::Backward, Vector2(0, 0.5f));
+    add(Vector3::Backward, Vector3::Up, Vector3::Left, Vector2(0.33f, 0.5f));
+    add(Vector3::Right, Vector3::Backward, Vector3::Down, Vector2(0.66f, 0.5f));
 
 
     return builder.Build();
@@ -30,12 +30,12 @@ Mesh* createFullScreenQuad()
 {
     MeshBuilder builder;
 
-    // d       c
-    //  +-----+
-    //  |   / |
-    //  | /   |
-    //  +-----+
     // a       b
+    //  +-----+
+    //  | \   |
+    //  |   \ |
+    //  +-----+
+    // d       c
 
     auto a = StandardVertex{Vector3(0, 0, 0), Vector3::Backward, Vector2(0, 0)};
     auto b = StandardVertex{Vector3(1, 0, 0), Vector3::Backward, Vector2(1, 0)};
